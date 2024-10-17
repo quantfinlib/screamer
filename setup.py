73561160d4b795673a0e9718b821619cb88e6d30
simu.ai <thijs@simu.ai>
2024-10-17 23:34:36 +0200

    changing build process

--- setup.py
+++ setup.py
@@ -1,56 +1,30 @@
-# setup.py
-
-from setuptools import setup
-from setuptools import Extension
-from setuptools.command.build_ext import build_ext
-import sys
-import pybind11
-import os
-
-class CMakeExtension(Extension):
-    def __init__(self, name):
-        super().__init__(name, sources=[])
-
-class CMakeBuild(build_ext):
-    def run(self):
-        for ext in self.extensions:
-            self.build_extension(ext)
-    
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cfg = 'Release'
-
-        cmake_args = [
-            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
-            f'-DCMAKE_BUILD_TYPE={cfg}',
-            f'-DPYTHON_EXECUTABLE={sys.executable}',
-        ]
-
-        build_args = []
-
-        if sys.platform.startswith('win'):
-            cmake_args += ['-A', 'x64']
-            build_args += ['--config', cfg]
-
-        build_temp = self.build_temp
-        if not os.path.exists(build_temp):
-            os.makedirs(build_temp)
-
-        os.chdir(build_temp)
-        self.spawn(['cmake', '..'] + cmake_args)
-        self.spawn(['cmake', '--build', '.', '--target', 'screamer_bindings'] + build_args)
-        os.chdir('..')
+from skbuild import setup
 
 setup(
-    name='screamer',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='Your package description',
-    long_description='Long description of your package',
-    packages=['screamer'],
-    ext_modules=[CMakeExtension('screamer_bindings')],
-    cmdclass={'build_ext': CMakeBuild},
-    zip_safe=False,
-    python_requires='>=3.7',
+    name="screamer",
+    version="0.1.0",
+    description="Screamingly fast streaming indicators with C++ performance and Python simplicity.",
+    long_description="Screamer is a high-performance Python library designed for efficient streaming indicator algorithms. Built with a core of optimized C++ code and integrated through Python bindings, Screamer delivers lightning-fast computations for real-time data processing. The library is perfect for real-time algorithmic trading applications that need low-latency indicators.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/quantfinlib/screamer",
+    author="Thijs van den Berg, Mohammadjavad Vakili",
+    author_email="thijs@sitmo.com, mohammadjavad.vakili@gmail.com",
+    packages=["screamer"],
+    cmake_install_dir="screamer",
+    python_requires='>=3.9',
+    keywords="streaming indicators Python C++ performance real-time algorithmic trading low-latency",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: C++",
+        "Operating System :: OS Independent",
+    ],
 )

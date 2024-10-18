faa5efd6e2dc444b3facc946930f692c199ce470
simu.ai <thijs@simu.ai>
2024-10-19 00:06:51 +0200

    Bump version: 0.1.14 → 0.1.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="screamer",
-    version="0.1.14",
+    version="0.1.15",
     description="Screamingly fast streaming indicators with C++ performance and Python simplicity.",
     long_description="Screamer is a high-performance Python library designed for efficient streaming indicator algorithms. Built with a core of optimized C++ code and integrated through Python bindings, Screamer delivers lightning-fast computations for real-time data processing. The library is perfect for real-time algorithmic trading applications that need low-latency indicators.",
     long_description_content_type="text/markdown",

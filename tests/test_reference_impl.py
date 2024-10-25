a89e923479fe31184b7236f524f589070e6b3608
simu.ai <thijs@simu.ai>
2024-10-25 17:08:33 +0200

    fixing failing import of reference impl  in github actions

--- tests/test_reference_impl.py
+++ tests/test_reference_impl.py
@@ -1,6 +1,12 @@
 import numpy as np
-import reference_impls
 import pytest
+import sys
+import os
+
+# Add the root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import reference_impls
 
 test_args = {
     'array': np.random.normal(size=50),

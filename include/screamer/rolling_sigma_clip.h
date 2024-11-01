1277e86c9b14483f6968d97cb08c68b095d8d299
simu.ai <thijs@simu.ai>
2024-11-01 12:05:33 +0100

    bump

---
+++ include/screamer/rolling_sigma_clip.h
@@ -0,0 +1,91 @@
+
+#ifndef SCREAMER_ROLLING_SIGMA_CLIP_H
+#define SCREAMER_ROLLING_SIGMA_CLIP_H
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <screamer/buffer.h>
+#include "screamer/base.h"
+#include "screamer/rolling_zscore.h"
+
+namespace py = pybind11;
+
+namespace screamer {
+
+    class RollingSigmaClip : public ScreamerBase {
+    public:
+
+        RollingSigmaClip(
+            int window_size,
+            std::optional<double> lower = std::nullopt, 
+            std::optional<double> upper = std::nullopt
+        ) : 
+            window_size_(window_size), 
+            sum_x_buffer(window_size),
+            sum_xx_buffer(window_size),
+            c0(1.0 / (window_size * (window_size - 1))),
+            lower_(lower), 
+            upper_(upper)
+        {
+            if (window_size <= 0) {
+                throw std::invalid_argument("Window size must be positive.");
+            }
+
+
+        }
+
+        void reset() override {
+            sum_x_buffer.reset();
+            sum_xx_buffer.reset();        
+        }
+        
+        double process_scalar(double newValue) override {
+            
+            double sum_x = sum_x_buffer.process_scalar(newValue);
+            double sum_xx = sum_xx_buffer.process_scalar(newValue * newValue);
+            double std = std::sqrt((window_size_ * sum_xx - sum_x * sum_x) * c0);
+            double mean = sum_x / window_size_;
+            return (newValue - mean) / std;
+        }
+
+        void process_array_no_stride(double* y, const double* x, size_t size) override {
+
+            size_t window_size_ = this->window_size_;
+            double sum_x = 0.0;
+            double sum_xx = 0.0;
+            double std_;
+            double mean_;
+
+            size_t split = std::min(size, window_size_);
+
+            for (size_t i=0; i<split; i++) {
+                sum_x += x[i];
+                sum_xx += x[i] * x[i];
+                std_ = std::sqrt((window_size_ * sum_xx - sum_x * sum_x) * c0);
+                mean_ = sum_x / window_size_;
+                y[i] = (x[i] - mean_) / std_;
+            }
+            
+            for (size_t i=split; i<size; i++) {
+                sum_x = sum_x + x[i] - x[i - window_size_];
+                sum_xx = sum_xx + x[i] * x[i] - x[i - window_size_] * x[i - window_size_];
+                std_ = std::sqrt((window_size_ * sum_xx - sum_x * sum_x) * c0); 
+                mean_ = sum_x / window_size_;
+                y[i] =  (x[i] - mean_) / std_;                           
+            }
+        }
+
+    private:
+        RollingSum sum_x_buffer;
+        RollingSum sum_xx_buffer;
+        const double c0;
+        const int window_size_;
+        double lower_;
+        double upper_;
+
+    }; // end of class
+
+} // end of namespace
+
+#endif // end of include guards
+

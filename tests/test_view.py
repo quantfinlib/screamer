88f28ecbb5641bae21c994b54c6e8241a92e9da8
simu.ai <thijs@simu.ai>
2024-11-03 13:38:41 +0100

    Added test for materialized strided view

--- tests/test_view.py
+++ tests/test_view.py
@@ -85,3 +85,35 @@ def test_screamer_view_strided_shifted(class_name, params, array_type, array_len
         screamer_output_1, screamer_output_2, rtol=1e-5, atol=1e-8,
         err_msg=f"Results do not match for {class_name}  with params {params} and array type '{array_type}' of length {array_length}"
     )
+
+
+# Create a pytest parameterization using the collected test cases
+@pytest.mark.parametrize(
+    "class_name, params, array_type, array_length",
+    yield_test_cases()
+)
+def test_screamer_view_strided_shifted_materialized(class_name, params, array_type, array_length):
+    """Compare the output of screamer class and baseline reference implementation."""
+    
+    # Get screamer and baseline classes
+    screamer_class = getattr(screamer_module, class_name, None)
+
+    # Instantiate the screamer and baseline objects
+    screamer_instance_1 = screamer_class(**params)
+    screamer_instance_2 = screamer_class(**params)
+
+    # Generate a 2N x 9 x 8 input matix
+    arrays = []
+    for _ in range(9*8):
+        arrays.append(generate_array(array_type, 2*array_length))
+    input_array = np.column_stack(arrays)
+    input_array = input_array.reshape((-1, 9, 8))
+    input_array_view = input_array[1::2, 2::3, 3::4]
+
+    screamer_output_1 = screamer_instance_2(input_array_view.copy())
+    screamer_output_2 = screamer_instance_2(input_array_view)
+
+    np.testing.assert_allclose(
+        screamer_output_1, screamer_output_2, rtol=1e-5, atol=1e-8,
+        err_msg=f"Results do not match for {class_name}  with params {params} and array type '{array_type}' of length {array_length}"
+    )
